aafba9b52c8ab9d980c5e0c9d75dba424715d6fb
Hansanghyeon <dandan9509@gmail.com>
2019-06-29 11:53:21 +0900

    Add: ScrollOverflow function

--- code/Babel/fullpage.js
+++ code/Babel/fullpage.js
@@ -9,18 +9,12 @@
 		navigation: true,
 		navigationPosition: 'pagei-sideNav-style1',
 		navigationTooltips: ['About me', 'Philosophy', 'Output', 'Contact', 'Instagram'],
+
+		// scroll
+		scrollOverflow: true,
 	});
 
 
-	$('.menu_about_me').on('click', function(){
-		$('#fp-nav ul li:nth-of-type(1) a')[0].click();
-	});
-	$('.menu_philosophy').on('click', function(){
-		$('#fp-nav ul li:nth-of-type(2) a')[0].click();
-	});
-	$('.menu_output').on('click', function(){
-		$('#fp-nav ul li:nth-of-type(3) a')[0].click();
-	});
 	$('.scroll-down.contact, .menu_contact').on('click', function(){
 		$('#fp-nav ul li:nth-of-type(4) a')[0].click();
 	});
