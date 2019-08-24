0c60ca72e35fd936d8f8845ae1bf65126dad7bb2
sanghyeon <dandan9509@gmail.com>
2019-08-24 17:35:29 +0900

    Fix: Prettier format

--- code/Babel/fullpage.js
+++ code/Babel/fullpage.js
@@ -1,24 +1,30 @@
-(function ($) {
+(function($) {
+  new fullpage(".home #et-boc", {
+    sectionsColor: ["#999", "#888", "#777", "#fff", "#555"],
+    parallax: true,
 
-	new fullpage('.home #et-boc', {
-		sectionsColor: ['#999', '#888', '#777', '#fff', '#555'],
-		parallax: true,
+    // Navigation
+    slidesNavigation: true,
+    navigation: true,
+    navigationPosition: "pagei-sideNav-style1",
+    navigationTooltips: [
+      "About me",
+      "Philosophy",
+      "Output",
+      "Contact",
+      "Instagram"
+    ],
 
-		// Navigation
-		slidesNavigation: true,
-		navigation: true,
-		navigationPosition: 'pagei-sideNav-style1',
-		navigationTooltips: ['About me', 'Philosophy', 'Output', 'Contact', 'Instagram'],
+    // scroll
+    scrollOverflow: true,
 
-		// scroll
-		scrollOverflow: true,
-	});
+    dragAndMove: true
+  });
 
-
-	$('.scroll-down.contact, .menu_contact').on('click', function(){
-		$('#fp-nav ul li:nth-of-type(4) a')[0].click();
-	});
-	$('.scroll-down.instagram').on('click', function(){
-		$('#fp-nav ul li:nth-last-of-type(1) a')[0].click();
-	});
+  $(".scroll-down.contact, .menu_contact").on("click", function() {
+    $("#fp-nav ul li:nth-of-type(4) a")[0].click();
+  });
+  $(".scroll-down.instagram").on("click", function() {
+    $("#fp-nav ul li:nth-last-of-type(1) a")[0].click();
+  });
 })(jQuery);
