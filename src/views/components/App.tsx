af46a847415b7a0f6f43e89ca4023775c6cc2fac
Hansanghyeon <dandan9509@gmail.com>
2020-03-09 10:21:41 +0900

    chore: fullpage setting

--- src/views/components/App.tsx
+++ src/views/components/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 export default () => {
   return (
-    <h1>
-      <a
-        href="https://github.com/JaeYeopHan/cra-template-unicorn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        cra-template-unicorn
-      </a>
-    </h1>
-  )
-}
+    <ReactFullpage
+      licenseKey={process.env.REACT_APP_FULLPAGE_LICENSEKEY}
+      scrollingSpeed={1000}
+      render={({ state, fullpageApi }: any) => (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      )}
+    />
+  );
+};
