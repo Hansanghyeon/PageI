9185fdcc44bbb248f0e1eae1768dd9c67a9b6fda
Hansanghyeon <dandan9509@gmail.com>
2020-03-14 11:13:37 +0900

    feat: non responsive design done

--- src/views/components/templates/GallerySection.tsx
+++ src/views/components/templates/GallerySection.tsx
@@ -5,26 +5,33 @@ import HoverOverCard from '@atom/card/HoverOver';
 
 const Section = styled.div`
   background-image: url(https://s3-ap-northeast-2.amazonaws.com/pagei/20190629131754/KakaoTalk_Photo_2019-05-25-13-58-27.jpeg);
-  .fp-tableCell {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-  }
+`;
+
+const InnerSection = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding-top: 100px;
+  padding-bottom: 200px;
 `;
 
 const GallerySection = () => (
   <Section className="section">
-    <HoverOverCard
-      imgSrc="https://s3-ap-northeast-2.amazonaws.com/pagei/20190715174256/Butterfleye6.jpg"
-      title="Butterfleye"
-      content="FASHION EYEWEAR"
-    />
-    <HoverOverCard
-      imgSrc="https://s3-ap-northeast-2.amazonaws.com/pagei/modulegoggle.jpg"
-      title="MODULAR"
-      content="SMART INDUSTRIAL SAFETY GOGGLE"
-    />
+    <InnerSection>
+      <HoverOverCard
+        imgSrc="https://s3-ap-northeast-2.amazonaws.com/pagei/20190715174256/Butterfleye6.jpg"
+        title="Butterfleye"
+        content="FASHION EYEWEAR"
+      />
+      <HoverOverCard
+        imgSrc="https://s3-ap-northeast-2.amazonaws.com/pagei/modulegoggle.jpg"
+        title="MODULAR"
+        content="SMART INDUSTRIAL SAFETY GOGGLE"
+      />
+    </InnerSection>
   </Section>
 );
 export default GallerySection;
--- src/views/pages/index.tsx
+++ src/views/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import ReactFullpage from '@fullpage/react-fullpage';
+import 'fullpage.js/vendors/scrolloverflow';
 
 import SEO from '@view/components/seo';
 import GallerySection from '@template/GallerySection';
 import BioSection from '@template/BioSection';
+import '@view/backup/Scss/fullpage.scss';
 
 const IndexPage = () => (
   <>
@@ -12,6 +13,17 @@ const IndexPage = () => (
     <ReactFullpage
       licenseKey={process.env.REACT_APP_FULLPAGE_LICENSE_KEY}
       scrollingSpeed={1000}
+      navigation
+      navigationPosition="pagei-sideNav-style1"
+      navigationTooltips={[
+        'Output',
+        'About me',
+        'Philosophy',
+        'Contact',
+        'Instagram',
+      ]}
+      dragAndMove
+      scrollOverflow
       render={({ state, fullpageApi }: any) => {
         return (
           <ReactFullpage.Wrapper>
