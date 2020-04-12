bd1a9f2a8419a59edbe8a9dd42d1ceaa753116c9
Hansanghyeon <dandan9509@gmail.com>
2020-04-12 17:51:11 +0900

    faet: 'Insight' 섹션 제작완료

--- src/views/pages/project/sound-spot.tsx
+++ src/views/pages/project/sound-spot.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 // Components
 import PlanIntroSection from '@template/sound-spot/Plan/Intro';
 import ProblemSection from '@template/sound-spot/Plan/Problem';
+import InsightSection from '@template/sound-spot/Plan/Insight';
 // Design System
 
 const soundSpot = () => (
   <>
     <PlanIntroSection />
     <ProblemSection />
+    <InsightSection />
   </>
 );
 
