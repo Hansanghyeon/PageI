f855ccb032e9c35b60a346ef18e0b5c1147752cf
Hansanghyeon <dandan9509@gmail.com>
2020-04-12 17:54:11 +0900

    chore: 불필요한 폴더 dept 제거

--- src/views/pages/project/sound-spot.tsx
+++ src/views/pages/project/sound-spot.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // Components
-import PlanIntroSection from '@template/sound-spot/Plan/Intro';
-import ProblemSection from '@template/sound-spot/Plan/Problem';
-import InsightSection from '@template/sound-spot/Plan/Insight';
+import PlanIntroSection from '@template/sound-spot/Intro';
+import ProblemSection from '@template/sound-spot/Problem';
+import InsightSection from '@template/sound-spot/Insight';
 // Design System
 
 const soundSpot = () => (
