b631bf50f029d0f2ead3aef6c4afe942f9d52c8d
Hansanghyeon <dandan9509@gmail.com>
2020-04-28 19:48:02 +0900

    feat: full screen 모드 추가

--- src/views/components/atoms/wrap/Image/index.tsx
+++ src/views/components/atoms/wrap/Image/index.tsx
@@ -1,14 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const ImageWrap = styled.div`
-  text-align: center;
-  background-color: #dbdbdb;
-  font-size: 0;
-  line-height: 0;
+export interface ImageWrapType {
+  bgColor?: string;
+  fullscreen?: boolean;
+}
+
+const nonFullscreen = css`
   img {
     width: 100%;
     margin-bottom: 0;
   }
+`;
+
+const onFullscreen = css`
+  height: 100vh;
+  min-height: 56%;
+  position: relative;
+  overflow: hidden;
+  img {
+    width: 100%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const ImageWrap = styled.div<ImageWrapType>`
+  text-align: center;
+  background-color: ${({ bgColor }) => (bgColor ? bgColor : '#dbdbdb')};
+  font-size: 0;
+  line-height: 0;
+  ${({ fullscreen }) => (fullscreen ? onFullscreen : nonFullscreen)};
 `;
 
 export default ImageWrap;
