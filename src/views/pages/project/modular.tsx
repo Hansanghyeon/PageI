796231dbf9967dc1db264f15d3a706b65f3ffcaf
Hansanghyeon <dandan9509@gmail.com>
2020-05-25 20:50:31 +0900

    feat: 글로벌 스타일로 해당 프로젝트의 베이스 스타일추가

--- src/views/pages/project/modular.tsx
+++ src/views/pages/project/modular.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 import IntroSection from '@template/modular/intro';
 import BackgroundSection from '@template/modular/background';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    --bg: ${({ theme }) => theme.project.modular.$bg};
+    --textColor: ${({ theme }) => theme.project.modular.$color};
+    background-color: var(--bg);
+    color: var(--textColor);
+    *, p {
+      font-size: 14px;
+    }
+  }
+  .tl-edges {
+    overflow: hidden;
+  }
+`;
 const Modular = () => (
   <>
+    <GlobalStyle />
     <IntroSection />
     <BackgroundSection />
   </>
