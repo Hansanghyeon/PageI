3b7d2f41070351735b13bfff8b813edebdff5d81
Hansanghyeon <dandan9509@gmail.com>
2020-09-30 15:28:57 +0900

    fix: next.js에서 더이상사용하지않는 요소

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps, Container } from 'next/app';
+import { AppProps } from 'next/app';
 // components
 import MainLayout from '@view/components/layouts/Main';
 import config from 'react-reveal/globals';
@@ -6,11 +6,9 @@ config({ ssrFadeout: true });
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </Container>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
   );
 }
 
