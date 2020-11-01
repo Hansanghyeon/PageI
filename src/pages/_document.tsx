381b6c6b9778835184f41737f8dbc89527198ca2
Hansanghyeon <dandan9509@gmail.com>
2020-11-01 16:50:27 +0900

    feat: next.js 권장사항 반영

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class _Document extends Document {
